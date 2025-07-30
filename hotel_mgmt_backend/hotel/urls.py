c694ca0591c315512c5744996216b186ef1c4a68
Harshad <harshadchavan92@gmail.com>
2025-07-30 20:41:01 +0530

    update and unique customer API

--- hotel_mgmt_backend/hotel/urls.py
+++ hotel_mgmt_backend/hotel/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import all_customer_list,insert_customer
+from .views import all_customer_list,insert_customer,single_customer,update_customer
 
 urlpatterns = [
     path('customer-list',all_customer_list,),
     path('insert',insert_customer,),
+    path('get/<int:id>',single_customer,),
+    path('update/<int:id>',update_customer,),
 ]
--- hotel_mgmt_backend/hotel/views.py
+++ hotel_mgmt_backend/hotel/views.py
@@ -11,13 +11,38 @@ from rest_framework import status
 def all_customer_list(request):
     queryset = Customer.objects.all()
     serializer = CustomerSerializer(queryset, many=True)
-    print('--------->>>',serializer.data) 
     return Response(serializer.data,status=200)
 
 @api_view(["POST"])
-def insert_customer(response):
-    serializer = CustomerSerializer(data = response.data)
+def insert_customer(request):
+    serializer = CustomerSerializer(data = request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data,status=status.HTTP_201_CREATED)
-    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST) 
+
+
+@api_view(["GET"])
+def single_customer(request,id):
+    try:
+        queryset = Customer.objects.get(id = id)
+        serializers = CustomerSerializer(queryset) 
+        return Response(serializers.data,status=status.HTTP_200_OK) 
+    except:
+        message = {"info":"Information not found for the mentioned ID"}
+        return Response(message,status=status.HTTP_404_NOT_FOUND) 
+    
+@api_view(["PATCH","PUT"])
+def update_customer(request,id):
+    try:
+        queryset = Customer.objects.get(id = id)
+        serializers = CustomerSerializer(queryset,data = request.data) 
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data,status=status.HTTP_200_OK) 
+        else:
+            message = {"info":serializers.error_messages}
+            return Response(message,status=status.HTTP_400_BAD_REQUEST)
+    except:
+        message = {"info":"Data not found for the given ID"}
+        return Response(message,status=status.HTTP_404_NOT_FOUND)
