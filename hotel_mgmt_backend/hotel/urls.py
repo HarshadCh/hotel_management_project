1e66c434696b95f27d5334cbc296419911119ea3
Harshad <harshadchavan92@gmail.com>
2025-07-31 20:44:20 +0530

    delete API created

--- hotel_mgmt_backend/hotel/urls.py
+++ hotel_mgmt_backend/hotel/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import all_customer_list,insert_customer,single_customer,update_customer
+from .views import all_customer_list,insert_customer,single_customer,update_customer,delete_customer,check_in_filter
 
 urlpatterns = [
     path('customer-list',all_customer_list,),
     path('insert',insert_customer,),
     path('get/<int:id>',single_customer,),
     path('update/<int:id>',update_customer,),
+    path('delete/<int:id>',delete_customer,),
+    path('check_in_filter/',check_in_filter,),
 ]
--- hotel_mgmt_backend/hotel/views.py
+++ hotel_mgmt_backend/hotel/views.py
@@ -46,4 +46,29 @@ def update_customer(request,id):
             return Response(message,status=status.HTTP_400_BAD_REQUEST)
     except:
         message = {"info":"Data not found for the given ID"}
+        return Response(message,status=status.HTTP_404_NOT_FOUND) 
+
+@api_view(["DELETE"])
+def delete_customer(request,id):
+    try:
+        queryset = Customer.objects.get(id = id) 
+        queryset.delete() 
+        message = {"info":"Customer has been deleted succesfully"}
+        return Response(message,status=status.HTTP_200_OK)
+    except:
+        message = {"info":"Data not found for the given ID"}
         return Response(message,status=status.HTTP_404_NOT_FOUND)
+         
+@api_view(["GET"])
+def check_in_filter(request):
+    incoming_date = request.query_params.get("check_in")
+    check_in_date = datetime.fromisoformat(incoming_date)
+    print(check_in_date,'---------->>>>')
+    datetime_object =  check_in_date.strftime('%d-%m-%Y')
+    print(datetime_object,'---------->>>>')
+    try:
+        queryset = Customer.objects.filter(check_in = datetime_object)
+        # serializers = CustomerSerializer(queryset)
+        # print(serializers.data,'----->>>')
+    except:
+        print("this is not working")
